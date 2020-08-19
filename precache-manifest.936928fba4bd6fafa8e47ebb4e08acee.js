54545b20dbe1a92efd32edbb4a3b42a9b5e5feaf
Startcev <Sergey56-11@mail.ru>
2020-08-19 21:01:42 +0500

    Add files via upload

---
+++ precache-manifest.936928fba4bd6fafa8e47ebb4e08acee.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a0926708503e365350fdf77f0509438c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "446a77f297b5891c2d61",
+    "url": "/static/css/2.1e562f28.chunk.css"
+  },
+  {
+    "revision": "1bff660f556e35e1fb8f",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "446a77f297b5891c2d61",
+    "url": "/static/js/2.9e3ae7ef.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.9e3ae7ef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1bff660f556e35e1fb8f",
+    "url": "/static/js/main.d1210183.chunk.js"
+  },
+  {
+    "revision": "880ead2686ec4d3085c1",
+    "url": "/static/js/runtime-main.b40e28e2.js"
+  }
+]);
